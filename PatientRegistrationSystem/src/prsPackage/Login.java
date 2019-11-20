b0d3724b42bc9c4dece1ad4bdd3e08b562e2bc8c
cHackogp <chackogp@gmail.com>
2019-11-20 00:03:43 -0500

    login with changes in hospitalmemeber test

--- PatientRegistrationSystem/src/prsPackage/Login.java
+++ PatientRegistrationSystem/src/prsPackage/Login.java
@@ -4,11 +4,31 @@ public class Login {
 	
 	String userName;
 	String password;
-	HospitalMember person;
+	int classID;
 	
-	public Login (String userName,String password) {
+	public Login (String userName,String password, int classID) {
 		this.userName = userName;
 		this.password = password;
+		this.classID = classID;
+	}
+	
+	public String getUserName()	{
+		return userName;
+	}
+	
+	protected void setUserName(String userName)	{
+		this.userName = userName;
+	}
+	
+	public String getPassword()	{
+		return password;
 	}
 
+	protected void setPassword(String password)	{
+		this.password = password;
+	}
+	
+	protected int getClassID()	{
+		return classID;
+	}
 }
