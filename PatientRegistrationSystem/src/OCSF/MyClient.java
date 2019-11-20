c8e63741b9228779228aa326ba2ace97b4f0c830
Administrator <Administrator@192.168.0.175>
2019-11-20 17:31:33 -0500

    MYSERVER IS DONE

--- PatientRegistrationSystem/src/OCSF/MyClient.java
+++ PatientRegistrationSystem/src/OCSF/MyClient.java
@@ -4,44 +4,44 @@ import java.io.IOException;
 
 import prsPackage.HospitalMember;
 
-public class MyClient extends AbstractClient{
+public class MyClient extends AbstractClient {
 
-	public MyClient(String host, int port)
-	  { 
+	public MyClient(String host, int port) {
 		super(host, port);
-	  }
-	
+	}
+
 	@Override
 	protected void handleMessageFromServer(Object msg) {
 		System.out.println("MyClient's handleMessageFromServer() triggered");
-		System.out.println(String.format("MyClient's handleMessageFromServer() : %s", msg));
+
 	}
-	
-	public Object clientRequest(String keyword, int methodIdentifier)	{
-		Objectinator object1 = new Objectinator(keyword,methodIdentifier);
+
+	public Object clientRequest(String keyword, int methodIdentifier) {
+		Objectinator object1 = new Objectinator(keyword, methodIdentifier);
 		return object1;
 	}
 
-	public static void main(String[] args) throws IOException , InterruptedException	{
+	public static void main(String[] args) throws IOException, InterruptedException {
 		MyClient Client1 = new MyClient("localhost", 8989);
 		try {
 			Client1.openConnection();
-		} catch (IOException e1) { 
+		} catch (IOException e1) {
 			e1.printStackTrace();
 			System.out.println("ERROR while attempting to open connection, exiting...");
 			System.exit(1); // on error exit
 		}
- 
-	    try { 
-	  //on close
-	    
-	    	System.out.println(String.format("Attempting to send message to server from %s %s %s", Client1.getHost(), Client1.getInetAddress(), Client1.getPort() ) );
-	    	Client1.sendToServer(clientRequest("Real Test",1));
+
+		try {
+			// on close
+
+			System.out.println(String.format("Attempting to send message to server from %s %s %s", Client1.getHost(),
+					Client1.getInetAddress(), Client1.getPort()));
+			Client1.sendToServer(clientRequest("Real Test", 1));
 
 			System.out.println("Client sent message to server");
-		} catch (IOException e) { 
+		} catch (IOException e) {
 			System.out.println("MyClient ERROR: sending to server did not work...");
 			e.printStackTrace();
-		}	    
+		}
 	}
 }
